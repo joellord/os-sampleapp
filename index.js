17961d354dc27aa2da1ac44028e394fe50d8a558
Joel Lord <jlord@localhost.localdomain>
2019-06-25 08:59:18 -0400

    Initial

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const app = express();
+
+const PORT = process.env.port || 8080;
+
+app.get("/", (req, res) => {
+    res.status(200).send("Hello World");
+});
+
+app.get("/health", (req, res) => {
+    res.status(200).send(`Healthy on ${new Date()}`);
+});
+
+app.listen(PORT, () => {
+    console.log(`Server started on port ${PORT}`)
+});
